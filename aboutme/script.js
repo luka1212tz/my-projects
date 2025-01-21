d309d4582f61a615cfc4e179f261980ad6c46af9
luka1212tz2 <15nlluksboy@gmail.com>
2025-01-21 23:21:15 +0400

    second commit

---
+++ aboutme/script.js
@@ -0,0 +1,19 @@
+
+let user = prompt("what device you use phone,tablet,laptop,pc")
+
+if(user == "phone" ){
+    window.location.href="./phonehome.html"
+}
+
+else if(user == "tablet" ){
+    window.location.href="./phonehome.html"
+}
+
+else if(user == "laptop" ){
+    window.location.href="./index.html"
+}
+
+else if(user == "pc" ){
+    window.location.href="./index.html"
+}
+
